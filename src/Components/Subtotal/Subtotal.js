8ac76ef5f4f146e861f1072b4e57182e951c183f
Priyanshi Gupta <priyanshigupta0708@gmail.com>
2020-10-30 12:54:04 +0530

    Subtotal made dynamic

--- src/Components/Subtotal/Subtotal.js
+++ src/Components/Subtotal/Subtotal.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import './Subtotal.css'
 import CurrencyFormat from 'react-currency-format'
+import { useStateValue } from '../../StateProvider'
 
 function Subtotal() {
+    // eslint-disable-next-line no-unused-vars
+    const [{ basket }, dispatch] = useStateValue()
+    let price = 0
+    basket.map((b) => (price = price + b.price))
+
     return (
         <div className="subtotal">
             <CurrencyFormat
                 renderText={(value) => (
                     <>
                         <p>
-                            Subtotal (0 items): <strong>0</strong>
+                            Subtotal ({basket?.length} items):{' '}
+                            <strong>{price}</strong>
                         </p>
                         <small className="subtotal__gift">
                             <input type="checkbox" />
