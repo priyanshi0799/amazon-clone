f6e88d4c529caeea435275cba687132dea0afc14
Priyanshi Gupta <priyanshigupta0708@gmail.com>
2020-10-30 12:43:08 +0530

    Checkout page added along with context API (REDUX)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import Header from './Components/Header/Header'
 import Home from './Components/Home/Home'
+import Checkout from './Components/Checkout/Checkout'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
     return (
-        <div>
-            <Header />
-            <Home />
-        </div>
+        <Router>
+            <div>
+                <Header />
+                <Switch>
+                    <Route path="/checkout">
+                        <Checkout />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
