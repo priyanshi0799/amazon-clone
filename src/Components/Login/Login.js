784ebe2a867094af64c9096308916508fe21f6a5
Priyanshi Gupta <priyanshigupta0708@gmail.com>
2020-10-30 21:01:57 +0530

    Firebase authentication added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,8 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import './Login.css'
+import { auth } from '../../firebase'
 
 function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const history = useHistory()
+
+    const signIn = (e) => {
+        e.preventDefault()
+        auth.signInWithEmailAndPassword(email, password)
+            .then((auth) => {
+                history.push('/')
+            })
+            .catch((error) => alert(error.message))
+    }
+
+    const register = (e) => {
+        e.preventDefault()
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                console.log(auth)
+            })
+            .catch((error) => alert(error.message))
+    }
+
     return (
         <div className="login">
             <Link to="/">
@@ -16,17 +40,33 @@ function Login() {
                 <h1>Sign-in</h1>
                 <form>
                     <h5>E-mail</h5>
-                    <input type="email" placeholder="johndoe@gmail.com" />
+                    <input
+                        type="email"
+                        placeholder="johndoe@gmail.com"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                     <h5>Password</h5>
-                    <input type="password" placeholder="*********" />
-                    <button className="login__signInButton">Sign In</button>
+                    <input
+                        type="password"
+                        placeholder="*********"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button
+                        type="submit"
+                        className="login__signInButton"
+                        onClick={signIn}
+                    >
+                        Sign In
+                    </button>
                 </form>
                 <p>
                     By signing-in you agree to Amazon's Conditions of Use &
                     Sale. Please see our Privacy Notice, our Cookies Notice and
                     out Interest-Based Ads Notice.
                 </p>
-                <button className="login__registerButton">
+                <button onClick={register} className="login__registerButton">
                     Create your Amazon account
                 </button>
             </div>
