60aee5dff38eda6291bc002922b1957822887944
Priyanshi Gupta <priyanshigupta0708@gmail.com>
2020-11-01 14:31:53 +0530

    Orders and Payment pages created

--- src/Components/CheckoutProduct/CheckoutProduct.js
+++ src/Components/CheckoutProduct/CheckoutProduct.js
@@ -3,7 +3,7 @@ import './CheckoutProduct.css'
 import Rating from '@material-ui/lab/Rating'
 import { useStateValue } from '../../Context/StateProvider'
 
-function CheckoutProduct({ id, image, title, price, rating }) {
+function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
     // eslint-disable-next-line no-unused-vars
     const [{ basket }, dispatch] = useStateValue()
 
@@ -26,7 +26,11 @@ function CheckoutProduct({ id, image, title, price, rating }) {
                 <div className="checkoutProduct__rating">
                     <Rating value={rating} max={5} precision={0.1} />
                 </div>
-                <button onClick={removeFromBasket}>Remove from Basket</button>
+                {hideButton && (
+                    <button onClick={removeFromBasket}>
+                        Remove from Basket
+                    </button>
+                )}
             </div>
         </div>
     )
--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { db } from '../../firebase'
+import { useStateValue } from '../../Context/StateProvider'
+import Order from './Order/Order'
 import './Orders.css'
 
 function Orders() {
+    const [orders, setOrders] = useState([0])
+    // eslint-disable-next-line no-unused-vars
+    const [{ basket, user }, dispatch] = useStateValue()
+
+    useEffect(() => {
+        if (user) {
+            db.collection('users')
+                .doc(user?.uid)
+                .collection('orders')
+                .orderBy('created', 'desc')
+                .onSnapshot((snapshot) => {
+                    setOrders(
+                        snapshot.docs.map((doc) => ({
+                            id: doc.id,
+                            data: doc.data(),
+                        }))
+                    )
+                })
+        } else {
+            setOrders([])
+        }
+    }, [])
+
     return (
         <div className="orders">
-            <h1>Orders</h1>
+            <h1>Your Orders</h1>
+            <div className="orders__order">
+                {orders?.map((order) => (
+                    <Order order={order} />
+                ))}
+            </div>
         </div>
     )
 }
